37b7cc95581d0f418027c2e90159c1667ff9bc21
anna anil <annaanil455@gmail.com>
2023-05-05 23:17:32 +0530

    first

---
+++ src/main/java/com/example/anildevops/AnilDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.anildevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnilDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnilDevopsApplication.class, args);
+	}
+
+}
