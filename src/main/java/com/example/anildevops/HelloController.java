23c78a2606bd00612532dc0390a64517b7d5f6b2
anna anil <45327725+Annaanil@users.noreply.github.com>
2023-05-14 16:32:41 +0530

    Update HelloController.java

--- src/main/java/com/example/anildevops/HelloController.java
+++ src/main/java/com/example/anildevops/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Anil devops-final-project java 17 succesfully done by me and successfully deployd in internet for accesing end user !!!!";
+        return "Anil devops-final-project java 17 succesfully done by me and successfully deployed in internet for accesing end user !!!!";
     }
 
  }
